1ac4080be95e4a54e1c1d0f58e2404ad304c3057
MetGang <patrick_stritch@wp.pl>
2020-05-12 20:08:52 +0200

    Added default values to Vertex class

--- Esdiel/include/Esdiel/Graphics/Vertex.hpp
+++ Esdiel/include/Esdiel/Graphics/Vertex.hpp
@@ -8,8 +8,8 @@ namespace esd
     ///
     struct Vertex
     {
-        Vec3f position;
-        Vec4f color;
-        Vec2f texCoord;
+        Vec3f position = { 0.0f, 0.0f, 0.0f };
+        Vec4f color = { 1.0f, 1.0f, 1.0f, 1.0f };
+        Vec2f texCoord = { 0.0f, 0.0f };
     };
 }
