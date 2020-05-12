0b487c59ffc0cd799183f078621404f3b2fee94e
MetGang <patrick_stritch@wp.pl>
2020-05-12 14:28:01 +0200

    Added Vertex class

---
+++ Esdiel/include/Esdiel/Graphics/Vertex.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// Esdiel
+#include <Esdiel/Math/Vector.hpp>
+
+namespace esd
+{
+    ///
+    struct Vertex
+    {
+        Vec2f position;
+        Vec4f color;
+        Vec2f texCoord;
+    };
+}
