a28672d44995a7467d8a854210da5004625286b3
MetGang <patrick_stritch@wp.pl>
2020-05-16 13:52:24 +0200

    Updated Window class

--- Esdiel/source/Esdiel/Graphics/Window.cpp
+++ Esdiel/source/Esdiel/Graphics/Window.cpp
@@ -68,6 +68,13 @@ namespace esd
                 if (gladLoadGL())
                 {
                     isGladLoaded = true;
+
+                    glEnable(GL_BLEND);
+                    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+                    glEnable(GL_CULL_FACE);
+                    glCullFace(GL_FRONT);
+                    glFrontFace(GL_CW);
                 }
                 else
                 {
