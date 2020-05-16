0c81ecc348019cbe92862111827e04eb93b0e67b
MetGang <patrick_stritch@wp.pl>
2020-05-16 12:56:05 +0200

    Added AnimatedSprite class

---
+++ Esdiel/include/Esdiel/Graphics/AnimatedSprite.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+// Esdiel
+#include <Esdiel/Graphics/VertexBuffer.hpp>
+#include <Esdiel/Graphics/Transformable.hpp>
+
+// C++
+#include <vector>
+
+namespace esd
+{
+    //
+    class Texture;
+
+    ///
+    class AnimatedSprite : public Transformable
+    {
+    public:
+
+        ///
+        AnimatedSprite();
+
+        ///
+        AnimatedSprite(AnimatedSprite const&) = delete;
+
+        ///
+        AnimatedSprite& operator = (AnimatedSprite const&) = delete;
+
+        ///
+        AnimatedSprite(AnimatedSprite&& rhs) noexcept;
+
+        ///
+        AnimatedSprite& operator = (AnimatedSprite&& rhs) noexcept;
+
+        ///
+        ~AnimatedSprite() = default;
+
+        ///
+        void SetTexture(Texture const& texture);
+
+        ///
+        Texture const& GetTexture() const;
+
+        ///
+        void SetColor(Vec4f const& color);
+
+        ///
+        Vec4f const& GetColor() const;
+
+        ///
+        void SetFrameSize(Vec2f const& frameSize);
+
+        ///
+        Vec2f const& GetFrameSize() const;
+
+        ///
+        void SetCurrentFrame(uint32_t index);
+
+        ///
+        uint32_t GetCurrentFrame() const;
+
+        ///
+        void SetFramesInRowCount(uint32_t count);
+
+        ///
+        uint32_t GetramesInRowCount() const;
+
+        ///
+        Vec4f GetLocalBounds() const;
+
+        ///
+        void Render(Window const& window, ShaderProgram const& shaderProgram, Camera const& camera) const;
+
+    private:
+
+        ///
+        void M_Update() const;
+
+        ///
+        void M_Defaultize();
+
+        mutable VertexBuffer m_vertexBuffer;
+        Texture const* m_texture;
+        Vec4f m_color;
+        Vec2f m_frameSize;
+        uint32_t m_currentFrame;
+        uint32_t m_framesInRowCount;
+    };
+}
---
+++ Esdiel/source/Esdiel/Graphics/AnimatedSprite.cpp
@@ -0,0 +1,142 @@
+#include <Esdiel/Graphics/AnimatedSprite.hpp>
+
+/// Esdiel
+#include <Esdiel/Graphics/Texture.hpp>
+
+namespace esd
+{
+    AnimatedSprite::AnimatedSprite()
+        : m_vertexBuffer { 4u, {}, PrimitiveType::TriangleFan }
+        , m_texture { nullptr }
+        , m_color { 1.0f, 1.0f, 1.0f, 1.0f }
+        , m_frameSize { 0.0f, 0.0f }
+        , m_currentFrame { 0 }
+        , m_framesInRowCount { 1 }
+    {
+
+    }
+
+    AnimatedSprite::AnimatedSprite(AnimatedSprite&& rhs) noexcept
+        : m_vertexBuffer{ std::move(rhs.m_vertexBuffer) }
+        , m_texture { rhs.m_texture }
+        , m_color { rhs.m_color }
+        , m_frameSize { rhs.m_frameSize }
+        , m_currentFrame { rhs.m_currentFrame }
+        , m_framesInRowCount { rhs.m_framesInRowCount }
+    {
+        
+    }
+
+    AnimatedSprite& AnimatedSprite::operator = (AnimatedSprite&& rhs) noexcept
+    {
+        if (this != &rhs)
+        {
+            m_vertexBuffer = std::move(rhs.m_vertexBuffer);
+            m_texture = rhs.m_texture;
+            m_color = rhs.m_color;
+            m_frameSize = rhs.m_frameSize;
+            m_currentFrame = rhs.m_currentFrame;
+            m_framesInRowCount = rhs.m_framesInRowCount;
+
+            rhs.M_Defaultize();
+        }
+
+        return *this;
+    }
+
+    void AnimatedSprite::SetTexture(Texture const& texture)
+    {
+        m_texture = &texture;
+    }
+
+    Texture const& AnimatedSprite::GetTexture() const
+    {
+        return *m_texture;
+    }
+
+    void AnimatedSprite::SetColor(Vec4f const& color)
+    {
+        m_color = color;
+    }
+
+    Vec4f const& AnimatedSprite::GetColor() const
+    {
+        return m_color;
+    }
+
+    void AnimatedSprite::SetFrameSize(Vec2f const& frameSize)
+    {
+        m_frameSize = frameSize;
+    }
+
+    Vec2f const& AnimatedSprite::GetFrameSize() const
+    {
+        return m_frameSize;
+    }
+
+    void AnimatedSprite::SetCurrentFrame(uint32_t index)
+    {
+        m_currentFrame = index;
+    }
+
+    uint32_t AnimatedSprite::GetCurrentFrame() const
+    {
+        return m_currentFrame;
+    }
+
+    void AnimatedSprite::SetFramesInRowCount(uint32_t count)
+    {
+        m_framesInRowCount = count;
+    }
+
+    uint32_t AnimatedSprite::GetramesInRowCount() const
+    {
+        return m_framesInRowCount;
+    }
+
+    Vec4f AnimatedSprite::GetLocalBounds() const
+    {
+        M_Update();
+
+        auto const bounds = m_vertexBuffer.GetBounds2D();
+        auto const pos = GetPosition();
+
+        return { bounds.x + pos.x, bounds.y + pos.y, bounds.z + pos.x, bounds.w + pos.y };
+    }
+
+    void AnimatedSprite::Render(Window const& window, ShaderProgram const& shaderProgram, Camera const& camera) const
+    {
+        M_Update();
+
+        m_texture->Bind(0);
+
+        m_vertexBuffer.Render(window, shaderProgram, camera, GetTransform());
+    }
+
+    void AnimatedSprite::M_Update() const
+    {
+        auto const texRect = Vec4f {
+            (m_currentFrame % m_framesInRowCount) * m_frameSize.x / m_texture->GetSize().x,
+            (m_currentFrame / m_framesInRowCount) * m_frameSize.y / m_texture->GetSize().y,
+            m_frameSize.x / m_texture->GetSize().x,
+            m_frameSize.y / m_texture->GetSize().y
+        };
+
+        auto vertices = m_vertexBuffer.GetData();
+
+        vertices[0] = { { 0.0f, 0.0f, 0.0f }, m_color, { texRect.x, texRect.y } };
+        vertices[1] = { { 0.0f + m_frameSize.x, 0.0f, 0.0f }, m_color, { texRect.x + texRect.z, texRect.y } };
+        vertices[2] = { { 0.0f + m_frameSize.x, 0.0f + m_frameSize.y, 0.0f }, m_color, { texRect.x + texRect.z, texRect.y + texRect.w } };
+        vertices[3] = { { 0.0f, 0.0f + m_frameSize.y, 0.0f }, m_color, { texRect.x, texRect.y + texRect.w } };
+    }
+
+    void AnimatedSprite::M_Defaultize()
+    {
+        m_vertexBuffer = { 4u, {}, PrimitiveType::TriangleFan };
+        m_texture = nullptr;
+        m_color = { 1.0f, 1.0f, 1.0f, 1.0f };
+        m_frameSize = { 0.0f, 0.0f };
+        m_currentFrame = 0;
+        m_framesInRowCount = 1;
+    }
+}
