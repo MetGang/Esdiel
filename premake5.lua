07dc1a32c07b11ace91312de93399795c3f2ed02
MetGang <patrick_stritch@wp.pl>
2020-05-19 02:34:44 +0200

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -28,18 +28,14 @@ workspace "E+G Study Project"
 -- Global variables
 targetDir = "%{cfg.system}/%{prj.name}/%{cfg.buildcfg}"
 
--- Project: Engine
-project "Engine"
+-- Main project
+project "Esdiel"
 
     location "Esdiel"
 
-    kind "StaticLib"
+    kind "WindowedApp"
 
-    targetprefix "lib"
-    targetname "engine"
-    targetextension ".a"
-
-    targetdir ("build/" .. targetDir .. "/lib")
+    targetdir ("build/" .. targetDir .. "/bin")
     objdir ("build/" .. targetDir .. "/obj")
 
     files {
@@ -55,32 +51,11 @@ project "Engine"
         "%{prj.location}/thirdparty/SDL2/include"
     }
 
--- Project: Game
-project "Game"
-
-    location "example"
-
-    kind "WindowedApp"
-
-    targetdir ("build/" .. targetDir .. "/bin")
-    objdir ("build/" .. targetDir .. "/obj")
-
-    files {
-        "%{prj.location}/main.cpp"
-    }
-
-    includedirs {
-        "%{wks.location}/Esdiel/include",
-        "%{wks.location}/Esdiel/thirdparty/",
-        "%{wks.location}/Esdiel/thirdparty/SDL2/include"
-    }
-
     libdirs {
-        "%{wks.location}/Esdiel/thirdparty/SDL2/lib"
+        "%{prj.location}/thirdparty/SDL2/lib"
     }
 
     links {
-        "engine",
         "SDL2main",
         "SDL2",
         "opengl32"
